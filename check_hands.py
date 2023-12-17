04a442fc782927faf3bafc85d197a1eac347e093
Binaryul <87132506+Binaryul@users.noreply.github.com>
2023-12-17 14:53:22 +0000

    completed check_hands.py
    All functions with this file are now completed and tested. Small tweaks may be done later. Now once the overall check_hand function is run from this file, the type of hand is returned along with which cards are the hand (excluding flushes which only return True

--- check_hands.py
+++ check_hands.py
@@ -1,60 +1,75 @@
 from deck import *
+from collections import Counter
 
-def straight_flush(hand):
-    pass
+def straight_flush(hand,value_list,suit_list):
+    #counts the occurrences of each suit
+    suit_counts = Counter(card.suit for card in hand)
+    #determins the most common card
+    most_common_suit = suit_counts.most_common(1)[0][0] #idk what any of these brackets do 
+    #makes a new list that only contains the cards of the most common suit
+    same_suit_cards = [card for card in hand if card.suit == most_common_suit]
+    
+    if len(same_suit_cards) >= 5:
+        values = [card.value for card in same_suit_cards]
+        straight_f = straight(hand,list(set(values)),suit_list)
+        if straight_f:
+            return sorted(straight_f,reverse=True)
 
-def four_kind(hand):
+def four_kind(hand,value_list,suit_list):
     #same as pair but with 4
-    ordered = sorted(hand, reverse=True)
+    ordered = sorted(value_list, reverse=True)
     for card in ordered:
         if ordered.count(card) == 4:
             return card
         
-def full_house(hand):
-    ordered = sorted(hand, reverse=True)
-    #first check for a 3ofAkind
+def full_house(hand,value_list,suit_list):
+    ordered = sorted(value_list, reverse=True)
+    full = []
+    #first check for a 3 of a kind
     #had to rewrite it here as the function doesn't remove the duplicates
     for card in ordered:
         if ordered.count(card) == 3:
+            full.extend([card,card,card])
             ordered = list(filter((card).__ne__, ordered))
             #if there is a 3 of a kind then it calls the pair function, if so then there is a full house
-            yes = pair(ordered)
+            yes = pair(hand,ordered,suit_list)
             if yes:
-                return True
+                full.extend([yes,yes])
+                return full
 
-def flush(hand):
-    for card in hand:
-        if hand.count(card) == 5:
+def flush(hand,value_list,suit_list):
+    for card in suit_list:
+        if suit_list.count(card) == 5:
             return True
 
-def straight(hand):
-    ordered = sorted(hand, reverse=True)
+def straight(hand,value_list,suit_list):
+    #makes a list that is ordered in decending order without any repeats
+    ordered = sorted(set(value_list), reverse=True)
     count = 1
     straight = []
-    #iterates through all items in the sorted list
-    for card in enumerate(ordered):
-        #if the next item = current + 1 increase the count
-        if ordered[card+1] == ordered[card] + 1:
+    #iterates through all items in the sorted list, it will start with the 2nd item
+    for card in range(1, len(ordered)):
+        #if the next item it 1 less then the current item then a straight starts
+        if ordered[card] == ordered[card - 1] - 1:
             count += 1
-            straight.append(ordered[card])
-            #if the count =5 then there are 5 items in numerical order
+            #append both current and next item
+            straight.extend([ordered[card- 1],ordered[card]])
             if count == 5:
-                #if there are a 5 items in numerical order, it returns the list of the straight to be compared later
-                return straight
+                #return the list without any duplicates
+                return list(set(straight))
         else:
-            #if the next item is not in numerical order then reset the count to 1
-            straight.clear
             count = 1
+            straight = []
         
-def three_kind(hand):
+def three_kind(hand,value_list,suit_list):
     #same as pair but with 3
-    ordered = sorted(hand, reverse=True)
+    ordered = sorted(value_list, reverse=True)
     for card in ordered:
         if ordered.count(card) == 3:
             return card
         
-def two_pair(hand):
-    ordered = sorted(hand, reverse=True)
+def two_pair(hand,value_list,suit_list):
+    ordered = sorted(value_list, reverse=True)
     count = 0
     #same as pair but will not stop after finding one pair and will go through every item in the list
     for card in ordered:
@@ -65,16 +80,16 @@ def two_pair(hand):
     if count == 2:
         return True
         
-def pair(hand):
+def pair(hand,value_list,suit_list):
     #orders the list of values in decending order
-    ordered = sorted(hand, reverse=True)
+    ordered = sorted(value_list, reverse=True)
     #goes through each item in the list starting from the largest and see if it has any repeats
     for card in ordered:
-        if ordered.count(card) == 2:
+        if ordered.count(card) >= 2:
             return card
 
-def high_card(hand):
-    return max(hand)
+def high_card(hand,value_list,suit_list):
+    return max(value_list)
 
 def check_hand(hand):
     #to check the hand, call this function which will then run other functions to then return the strongest hand
@@ -84,17 +99,10 @@ def check_hand(hand):
     for card in hand:
         value_list.append(card.value)
         suit_list.append(card.suit)
-    t = full_house(value_list)
-    return t
-
-
-
-
-
-#test
-hand = [Card(2,"Clubs"),Card(2,"Clubs"),Card(2,"Clubs"),Card(3,"Clubs"),Card(3,"Diamond")]
-
-t = check_hand(hand)
-if t == True:
-    print("skdfjh")
+    #list of all hand check functions
+    hand_checks = [straight_flush, four_kind, full_house, straight, three_kind, two_pair, pair, high_card]
     
+    for poker_check in hand_checks:
+        result = poker_check(hand,value_list,suit_list)
+        if result:
+            return poker_check.__name__ , result
---
+++ test_hands.py
@@ -0,0 +1,25 @@
+from check_hands import *
+
+test_hands = [
+    [Card(2, "Hearts"), Card(5, "Diamonds"), Card(10, "Hearts"), Card(13, "Spades"), Card(3, "Hearts"), Card(8, "Clubs"), Card(14, "Diamonds")],
+    [Card(4, "Clubs"), Card(7, "Hearts"), Card(13, "Diamonds"), Card(12, "Spades"), Card(2, "Hearts"), Card(6, "Clubs"), Card(9, "Diamonds")],
+    [Card(3, "Spades"), Card(6, "Diamonds"), Card(9, "Clubs"), Card(12, "Hearts"), Card(8, "Hearts"), Card(11, "Diamonds"), Card(10, "Spades")],
+    [Card(5, "Clubs"), Card(6, "Hearts"), Card(8, "Diamonds"), Card(10, "Spades"), Card(9, "Hearts"), Card(7, "Spades"), Card(12, "Clubs")],
+    [Card(2, "Diamonds"), Card(4, "Spades"), Card(7, "Clubs"), Card(10, "Diamonds"), Card(11, "Hearts"), Card(13, "Hearts"), Card(14, "Clubs")],
+    [Card(3, "Diamonds"), Card(5, "Hearts"), Card(8, "Hearts"), Card(10, "Diamonds"), Card(6, "Spades"), Card(12, "Clubs"), Card(13, "Diamonds")],
+    [Card(4, "Hearts"), Card(6, "Hearts"), Card(8, "Diamonds"), Card(10, "Clubs"), Card(11, "Spades"), Card(12, "Hearts"), Card(13, "Spades")],
+    [Card(2, "Clubs"), Card(4, "Diamonds"), Card(6, "Hearts"), Card(8, "Spades"), Card(10, "Hearts"), Card(12, "Diamonds"), Card(14, "Spades")],
+    [Card(3, "Hearts"), Card(5, "Clubs"), Card(7, "Diamonds"), Card(9, "Spades"), Card(11, "Hearts"), Card(13, "Clubs"), Card(14, "Diamonds")],
+    [Card(2, "Spades"), Card(4, "Hearts"), Card(6, "Clubs"), Card(8, "Diamonds"), Card(10, "Hearts"), Card(12, "Spades"), Card(13, "Diamonds")],
+    [Card(2, "Hearts"), Card(3, "Hearts"), Card(4, "Hearts"), Card(5, "Hearts"), Card(6, "Hearts"), Card(7, "Hearts"), Card(8, "Hearts")],
+    [Card(14, "Hearts"), Card(14, "Diamonds"), Card(14, "Clubs"), Card(13, "Hearts"), Card(13, "Diamonds"), Card(13, "Spades"), Card(2, "Hearts")],
+    [Card(10, "Spades"), Card(11, "Spades"), Card(12, "Spades"), Card(13, "Spades"), Card(14, "Spades"), Card(2, "Hearts"), Card(3, "Diamonds")],
+    [Card(3, "Hearts"), Card(3, "Diamonds"), Card(3, "Clubs"), Card(5, "Hearts"), Card(5, "Diamonds"), Card(10, "Spades"), Card(13, "Hearts")],
+    [Card(2, "Hearts"), Card(4, "Diamonds"), Card(6, "Hearts"), Card(8, "Spades"), Card(10, "Hearts"), Card(12, "Diamonds"), Card(14, "Spades")],
+    [Card(2, "Spades"), Card(3, "Hearts"), Card(5, "Clubs"), Card(7, "Diamonds"), Card(10, "Hearts"), Card(12, "Spades"), Card(13, "Diamonds")], 
+    [Card(2, "Hearts"), Card(3, "Hearts"), Card(4, "Hearts"), Card(5, "Hearts"), Card(6, "Hearts"), Card(7, "Clubs"), Card(8, "Clubs")], 
+    [Card(10, "Hearts"), Card(11, "Diamonds"), Card(12, "Spades"), Card(13, "Hearts"), Card(14, "Spades"), Card(2, "Hearts"), Card(3, "Diamonds")]
+]
+
+for i in test_hands:
+    print(check_hand(i))
